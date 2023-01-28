ffce03f2037afa14f0e330f4e0350f49805cc3eb
Vantonage <oshawot77@gmail.com>
2023-01-28 00:01:16 -0500

    Changed the HTTP response to use createSuccessResponse for index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ const router = express.Router();
 // modifications to src/routes/index.js
 // Our authentication middleware
 const { authenticate } = require('../authorization');
+const { createSuccessResponse } = require('../response');
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -23,11 +24,12 @@ router.use(`/v1`, authenticate(), require('./api'));
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 router.get('/', (req, res) => {
+  const successResponse = createSuccessResponse();
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
   res.status(200).json({
-    status: 'ok',
+    status: successResponse.status,
     author,
     // Use your own GitHub URL for this...
     githubUrl: 'https://github.com/Vantonage/fragments',
