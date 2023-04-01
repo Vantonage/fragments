c5c88650099a9d08c08a2e3b612ca8a9b3869672
Vantonage <oshawot77@gmail.com>
2023-04-01 18:02:56 -0400

    Added route to delete fragments

---
+++ src/routes/api/delete.js
@@ -0,0 +1,19 @@
+// src/routes/api/delete.js
+const { createErrorResponse, createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
+
+/**
+ * Delete a fragment by id
+ */
+module.exports = async (req, res) => {
+  try {
+    logger.debug(`Deleting fragment of this id ${req.params._id}`);
+    var id = req.params._id;
+    await Fragment.delete(req.user, id);
+    res.status(200).json(createSuccessResponse());
+  } catch (err) {
+    logger.error(err);
+    res.status(404).json(createErrorResponse(404, 'Fragment not found'));
+  }
+};
