4e0d825d045088fe44b1683b5379271d204c6ea9
Vantonage <oshawot77@gmail.com>
2023-04-18 16:28:14 -0400

    Adding the getById.test.js. The conversion test currently does not do anything as I have no impelemented the conversion part

--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -26,4 +26,19 @@ describe('getById /v1/fragments/:_id', () => {
     const res = await request(app).get('/v1/fragments/123').auth('user1@email.com', 'password1');
     expect(res.statusCode).toBe(404);
   }); 
+
+  // Using a valid username/password pair with correct extension
+  test('authenticated users get a fragments array', async () => {
+    const res = await request(app)
+    .post('/v1/fragments')
+    .auth('user1@email.com', 'password1')
+    .set('Content-Type', 'text/plain')
+    .send(data);
+    var id = JSON.parse(res.text).fragment.id;
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1')
+    expect(getRes.statusCode).toBe(200);
+    //expect(getRes.text).toBe(data.toString());
+  });
 });
